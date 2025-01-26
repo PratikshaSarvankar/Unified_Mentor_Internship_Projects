f876f8b9b185e062ac28a59b62197e45ed79a15a
PratikshaSarvankar <159712961+PratikshaSarvankar@users.noreply.github.com>
2025-01-26 11:19:51 +0530

    Update firebaseConfig.js

--- Student-Teacher Booking Appointment/firebaseConfig.js
+++ Student-Teacher Booking Appointment/firebaseConfig.js
@@ -1,13 +1,5 @@
 // Firebase configuration for the project
-var firebaseConfig = {
-  apiKey: "AIzaSyC6FTLhB02p04g97KPNCkxjzUkCLTtVx_Y",
-  authDomain: "student-teacher-booking-dccca.firebaseapp.com",
-  projectId: "student-teacher-booking-dccca",
-  storageBucket: "student-teacher-booking-dccca.appspot.com",
-  messagingSenderId: "681670673624",
-  appId: "1:681670673624:web:someappid",
-  measurementId: "G-measurement-id"
-};
+
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
